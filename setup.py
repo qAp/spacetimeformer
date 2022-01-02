d1e22b21f0136975605d90be17b799b21dc39a28
Jack Yu <jackchungchiehyu@gmail.com>
2022-01-02 17:22:57 +0800

    Just do setup() in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="spacetimeformer",
-    version="0.1.0",
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    description="QData Research Library for the Spacetimeformer project",
-    author="Jake Grigsby",
-    author_email="jcg6dn@virginia.edu",
-    license="MIT",
-    packages=find_packages(),
-)
+setup()
+
